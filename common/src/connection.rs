245481df8b6e5cd7118f3309a050faa616045356
axelkelman <akelman@fi.uba.ar>
2022-11-25 00:54:46 -0300

    Agrego paquete de información + respuestas del server

--- common/src/connection.rs
+++ common/src/connection.rs
@@ -1,18 +1,20 @@
-use std::{io::Read, net::TcpStream, sync::{Arc, mpsc::Sender}};
+use std::{io::{Read, Write}, net::TcpStream, sync::{Arc, mpsc::Sender}};
 
-use crate::{data_base::DataBase, packet_type::PacketType, payment::Payment, register::Register, vote::Vote};
+use crate::{data_base::DataBase, packet_type::PacketType, payment::Payment, register::Register, vote::Vote, infopacket::InfoPacket};
+
+static VOTE_COST: u32 = 100;
 
 // Almacenar datos de la conexión
 pub struct Connection {
     stream: TcpStream,
-    //logged: bool,
+    username: Option<String>
 }
 
 impl Connection {
     pub fn new(stream: TcpStream) -> Connection {
         Connection {
             stream,
-            //logged: false,
+            username: None
         }
     }
 
@@ -35,38 +37,58 @@ impl Connection {
                     self.handler_payment(Payment::from_bytes(buffer.to_vec()), &data_base)
                 },
                 PacketType::VOTE => {
-                    println!("Se recibio un pago");
+                    println!("Se recibio un voto");
                     self.handler_vote(Vote::from_bytes(buffer.to_vec()), &data_base, tx.clone())
                 },
+                PacketType::INFO => {
+                    println!("Se solicitaron los nominados");
+                    self.handler_request(&data_base,tx.clone())
+                }
                 _ => (),
             }
             buffer = [0; 1024];
         }
     }
 
-    pub fn handler_register(&self, packet: Register, data_base: &Arc<DataBase>) {
-        data_base
-            .save_new_user(packet.username, packet.password, packet.email)
-            .unwrap();
-        println!("Se registro correctamente al cliente");
-        // Aca yo diria de ver si es error(mandar error) o mandar ok
-    }
-
-    pub fn handler_payment(&self, packet: Payment, data_base: &Arc<DataBase>) {
-        data_base
-            .update_money(packet.username, packet.amount)
-            .unwrap();
-        println!("Se recargo correctamente saldo")
-        // Lo mismo aca
-    }
-
-    pub fn handler_vote(&self, packet: Vote, data_base: &Arc<DataBase>, tx: Sender<Vote>) {
-       
-        // eventualmente hay qe validar el voto con la informacion de la data base
-        // x ahora con esto aplico la chaneleta
-        if let Err(_) = tx.send(packet){
-            println!("Vote send to main thread failed")
+    pub fn handler_register(&mut self, packet: Register, data_base: &Arc<DataBase>) {
+        if let Err(e) =  data_base.save_new_user(packet.username.clone(), packet.password, packet.email) {
+            self.write_error(&e);
+            return;
         }
-        // Lo mismo aca
+        self.username = Some(packet.username);    
+        println!("Se registro correctamente al cliente");        
+    }
+
+    pub fn handler_payment(&mut self, packet: Payment, data_base: &Arc<DataBase>) {
+        if let Err(e) =   data_base.update_money(packet.username, packet.amount) {
+            self.write_error(&e);
+            return;
+        }
+            
+        println!("Se recargo correctamente saldo")
+    }
+
+    pub fn handler_vote(&mut self, packet: Vote, data_base: &Arc<DataBase>, tx: Sender<Vote>) {
+        if self.username.is_some(){
+            if let Err(e) = data_base.can_vote(self.username.as_deref().unwrap(), VOTE_COST * packet.cantidad_votos as u32) {
+                self.write_error(&e);
+                return; 
+            }
+            if tx.send(packet).is_err(){
+                self.write_error("SERVER FATAL ERROR");
+            }
+        }        
+        
+        
+        
+    }
+
+    pub fn handler_request(&mut self,data_base: &Arc<DataBase>, tx: Sender<Vote>) {
+
+    }
+
+    pub fn write_error(&mut self,error_msg: &str)  {
+        let error_pkt = InfoPacket::new(PacketType::ERROR,error_msg.to_string()).to_bytes();
+        self.stream.write(&error_pkt).unwrap(); // Consultar que hacemos aca  
     }
 }
---
+++ common/src/infopacket.rs
@@ -0,0 +1,63 @@
+use crate::packet_type::PacketType;
+
+pub struct InfoPacket {
+    packet_type: PacketType,
+    info_msg_size: u8,
+    info_msg : String,  
+}
+
+impl InfoPacket {
+    pub fn new(pkt_type: PacketType,info_msg: String) -> InfoPacket {
+        let info_msg_size = info_msg.len() as u8;
+        InfoPacket {
+            packet_type: pkt_type,
+            info_msg_size,
+            info_msg    
+        }
+    }
+
+    pub fn to_bytes(&mut self) -> Vec<u8> {
+        let packet_type_bytes = self.packet_type.as_utf8().to_be_bytes().to_vec();
+        let info_msg_size_bytes = self.info_msg_size.to_be_bytes().to_vec();
+        let info_msg_bytes = self.info_msg.as_bytes().to_vec();
+        [
+            packet_type_bytes,
+            info_msg_size_bytes,
+            info_msg_bytes,
+        ]
+        .concat()
+    }
+
+    pub fn from_bytes(bytes: Vec<u8>) -> InfoPacket {
+        let mut i = 0;
+        let pkt_type = PacketType::from_utf8(bytes[0]); 
+        let info_msg_size = bytes[1] as usize;
+        i += info_msg_size + 2;
+        let info_msg = String::from_utf8(bytes[2..i].to_vec()).unwrap();        
+        InfoPacket::new(pkt_type,info_msg)
+    }
+}
+
+#[cfg(test)]
+
+mod info_packet_test {
+    use crate::packet_type::PacketType;
+
+    use super::InfoPacket;
+
+    #[test]
+    fn info_packet_to_bytes_test() {
+        let mut test_packet = InfoPacket::new(PacketType::ERROR,"error".to_string());
+
+        let expected = vec![4,5,101,114,114,111,114];
+        assert_eq!(test_packet.to_bytes(), expected);
+    }
+
+    #[test]
+    fn info_packet_from_bytes_test() {
+        let bytes = vec![4,10,102,97,116,97,108,101,114,114,111,114];
+
+        let pkt = InfoPacket::from_bytes(bytes);
+        assert_eq!(pkt.info_msg, "fatalerror".to_string());        
+    }
+}
